7534e1598be4df655643b2b1bdb11587a890b977
Julian Wiche <julian@wiche.eu>
2022-12-24 07:07:07 +0100

    modified:   onkyo-ri-codes.js

---
+++ onkyo-ri-codes.js
@@ -0,0 +1,24 @@
+module.exports = function(RED) {
+    function OnkyoRiCodes(config) {
+
+    const fs = require('fs');
+    const path = require('path');
+
+    const basename = path.basename(__filename);
+    const functions = {}
+
+    fs
+    .readdirSync("./devices/")
+    .filter(file => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-6) === '.jason'))
+    .map((file) => {functions[file.slice(0, -6)] = require(path.join(__dirname, file})))
+
+
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.on('input', function(msg) {
+            msg.payload = msg.payload.toLowerCase();
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("onkyo-ri-codes",OnkyoRiCodes);
+}
